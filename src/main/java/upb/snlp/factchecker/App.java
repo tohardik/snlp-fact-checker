ec3872ef617f1434cd9d86f5924c3867427ead3c
Nandeesh <nandeeshpatelgp@gmail.com>
2019-01-27 22:50:01 +0100

    Removing commented code.

--- src/main/java/upb/snlp/factchecker/App.java
+++ src/main/java/upb/snlp/factchecker/App.java
@@ -9,7 +9,6 @@ import upb.snlp.factchecker.util.TSVReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class App {
     private static final String FACT_URI = "<http://swc2017.aksw.org/task2/dataset/";
@@ -17,13 +16,6 @@ public class App {
     private static final String TYPE = "<http://www.w3.org/2001/XMLSchema#double>";
     public static void main(String[] args) throws IOException {
         List<Fact> inputs = TSVReader.getFactsFromFile("src/main/resources/test.tsv");
-        /*List<Fact> inputs = new ArrayList<>();
-        Fact fact = new Fact();
-        fact.setTruthfulnessValue(0.0f);
-        fact.setTruthfulness(false);
-        fact.setFactId(3610545);
-        fact.setStatement("Stars Lisa Spoonhauer has been clerked Clerks.");
-        inputs.add(fact);*/
         FileWriter fw = new FileWriter("src/main/resources/result.ttl");
         for(Fact i : inputs) {
             RDFTriple triplet;
